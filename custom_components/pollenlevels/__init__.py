eaad441c1f7d2d5b85a5562b378759b2e04dde83
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-05-16 21:13:11 +0200

    Update __init__.py

--- custom_components/pollenlevels/__init__.py
+++ custom_components/pollenlevels/__init__.py
@@ -1,3 +1,20 @@
-""" Home Assistant Pollen Levels Integration """
+"""Home Assistant Pollen Levels integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-__version__ = "1.0.0"
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration (no YAML support)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the integration from a config entry."""
+    # Forward the setup to the sensor platform
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return True
