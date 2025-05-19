005a5770576b53a969793f22f70966d6021ecb54
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-05-19 07:24:29 +0200

    Update const.py

--- custom_components/pollenlevels/const.py
+++ custom_components/pollenlevels/const.py
@@ -1,5 +1,4 @@
 # Constants for Pollen Levels integration
-
 DOMAIN = "pollenlevels"
 CONF_API_KEY = "api_key"
 CONF_LATITUDE = "latitude"
@@ -7,8 +6,8 @@ CONF_LONGITUDE = "longitude"
 CONF_ALLERGENS = "allergens"
 CONF_UPDATE_INTERVAL = "update_interval"
 
-# Solo los tres tipos de PollenType soportados por Google Maps Pollen API
+# Supported pollen types by Google Maps Pollen API
 ALLERGEN_OPTIONS = ["GRASS", "TREE", "WEED"]
 
-# Intervalo por defecto en horas
+# Default update interval in hours
 DEFAULT_UPDATE_INTERVAL = 6
