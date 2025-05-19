c05aad55cc2cb5009863a6e01eb250fac038e570
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-05-19 07:24:00 +0200

    Update __init__.py

--- custom_components/pollenlevels/__init__.py
+++ custom_components/pollenlevels/__init__.py
@@ -3,13 +3,13 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import DOMAIN
+DOMAIN = "pollenlevels"
 
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the integration (no YAML support)."""
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up the integration from a config entry."""
     try:
         await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
@@ -17,7 +17,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         raise ConfigEntryNotReady from err
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return True
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
